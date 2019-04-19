7d17c35e5bacc518e90bda8da234955782d0c262
Dmitriy <d.komerzan@gmail.com>
2019-04-19 07:02:06 -0400

    Got the HTML for my calculator and formating mostly done. Need to focus on the C# code now

---
+++ models/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+namespace WebApp.ModelProd
+    {
+public class Product
+{
+    public int Id { get; set; }
+    public string ProdName { get; set; }
+    public string Desc { get; set; }
+    public decimal Cost { get; set; }
+    public string Img { get; set; }
+}
+
+}
