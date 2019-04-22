b185a80e92657a425190d5a6fcd763d2dd9624a9
Dmitriy <d.komerzan@gmail.com>
2019-04-22 00:10:17 -0400

    HTML Code finally talking to C# code but broke HTML styling in the process. Will have to figure out what went wrong

--- Pages/Calculator.cshtml.cs
+++ Pages/Calculator.cshtml.cs
@@ -9,8 +9,40 @@ namespace MyApp.Namespace
 {
     public class CalculatorModel : PageModel
     {
+		
+
+		[TempData]
+        public string ResultInfo { get; set; }
+       
+     
+        [BindProperty]
+        public double UsrInput { get; set; }
+
+       
+        [BindProperty]
+        public double OperandInput { get; set; }
+		
+		
+		
+		
         public void OnGet()
-        {
+        {	
         }
+
+        public void OnPostCalculate(String BtnVal)
+        {
+
+
+            ResultInfo = BtnVal;
+
+            ViewData["result"] = $"{ResultInfo}";
+
+
+
+
+
+
+        }
+
     }
 }
