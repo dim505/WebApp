a96eb4e0a9c2f4647a3a5bd79557c577919cfa56
Dmitriy <d.komerzan@gmail.com>
2019-06-19 23:21:14 -0400

    added a website preloader and updated site with one more react JS project

--- main_portfolio/Pages/ReactJsWeatherApp.cshtml.cs
+++ main_portfolio/Pages/ReactJsWeatherApp.cshtml.cs
@@ -1,13 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace razor_pages.Pages
+namespace MyApp.Namespace
 {
-    public class PrivacyModel : PageModel
+    public class ReactJsWeatherAppModel : PageModel
     {
         public void OnGet()
         {
